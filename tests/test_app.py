20c8692d56bbb94df7300420bf2613e41a53b75c
Lucas Medeiros <lcsmedeiroscosta@gmail.com>
2024-10-19 23:16:52 -0300

    iniciando o projeto
    Signed-off-by: Lucas Medeiros <lcsmedeiroscosta@gmail.com>

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_root_deve_retornar_ok_e_ola_mundo():
+
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+# estratégia conhecida como AAA, que divide o teste em três fases distintas: Arrange, Act, Assert.
